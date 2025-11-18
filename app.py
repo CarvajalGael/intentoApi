778b2a076281b34b3718971cbc0bed588f939f56
CarvajalGael <23308060610312@cetis61.edu.mx>
2025-11-18 12:50:26 -0500

    pochaimon

--- app.py
+++ app.py
@@ -18,34 +18,39 @@ def recetas():
     params = {
         'apiKey': api_key,
         'diet': 'vegan',  
-        'maxReadyTime': 30,  
-        'number': 5, 
-        'instructionsRequired': True, 
-        'minCalories': 200,  
-        'maxCalories': 500,  
-        'addRecipeInformation': True,  
-        'sort': 'calories',  
+        'maxReadyTime': 30,
+        'number': 5,
+        'instructionsRequired': True,
+        'minCalories': 200,
+        'maxCalories': 500,
+        'addRecipeInformation': True,
+        'sort': 'calories',
     }
 
     if request.method == 'POST':
+        dieta = request.form.get('dieta')
         max_tiempo = request.form.get('max_tiempo')
         calorias_min = request.form.get('calorias_min')
         calorias_max = request.form.get('calorias_max')
 
         if dieta:
             params['diet'] = dieta
+
         if max_tiempo:
             params['maxReadyTime'] = int(max_tiempo)
+
         if calorias_min:
             params['minCalories'] = int(calorias_min)
+
         if calorias_max:
             params['maxCalories'] = int(calorias_max)
+
 
     response = requests.get(API, params=params)
 
     if response.status_code == 200:
         data = response.json()
-        recetas_data = data['results']
+        recetas_data = data.get('results', [])
     else:
         flash("Error al obtener las recetas. Intenta nuevamente.", 'danger')
 
